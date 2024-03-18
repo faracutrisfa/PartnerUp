e09dbc65ca6ee82b63169ba608ba460429a9bf76
faracutrisfa <faracutrisfa@gmail.com>
2024-03-18 10:42:31 +0700

    login register update

--- src/components/Auth/BiodataForm.jsx
+++ src/components/Auth/BiodataForm.jsx
@@ -6,6 +6,10 @@ const BiodataForm = () => {
         <div className="relative flex justify-center items-center">
             <img src={Elemen} alt="Element Landing Page " className="w-full -mt-16" />
             <div className="w-[532px] absolute -mt-32">
+                <form>
+                    <input 
+                    />
+                </form>
             </div>
         </div>
     )
