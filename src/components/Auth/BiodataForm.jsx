1f5297dbd61e2192a23398b4e24c35482835e6e0
faracutrisfa <faracutrisfa@gmail.com>
2024-03-19 16:00:43 +0700

    biodata update

--- src/components/Auth/BiodataForm.jsx
+++ src/components/Auth/BiodataForm.jsx
@@ -3,12 +3,62 @@ import Elemen from "../../assets/image/ElemenRegister.svg"
 
 const BiodataForm = () => {
     return (
-        <div className="relative flex justify-center items-center">
+        <div className="relative flex justify-center items-center font-Poppins">
             <img src={Elemen} alt="Element Landing Page " className="w-full -mt-16" />
             <div className="w-[532px] absolute -mt-32">
                 <form>
+                    <label htmlFor="name" className="text-black text-base font-medium">Nama Lengkap</label>
                     <input 
+                        type="text" 
+                        id="name"
+                        className="mt-[16px] w-[532px] h-[67px] px-9 py-7 bg-purple-50 rounded-3xl border border-purple-900 justify-start items-center gap-2 inline-flex text-purple-900" 
                     />
+                    <label htmlFor="name" className="text-black text-base font-medium flex mt-[24px]">Asal Perguruan Tinggi</label>
+                    <select name="universitas" id="universitas" required className="">
+                        <option value="" disabled selected>Pilih Perguruan Tinggi</option>
+                        <option value="IBUM">IKIP Budi Utomo Malang</option>
+                        <option value="IPM">Institut Pertanian Malang</option>
+                        <option value="ITBAM">Institut Teknologi dan Bisnis Asia Malang</option>
+                        <option value="ITNM">Institut Teknologi Nasional Malang</option>
+                        <option value="POLINEMA">Politeknik Negeri Malang</option>
+                        <option value="STIE-M">STIE Malangkucecwara</option>
+                        <option value="STIKI-M">STIKI Malang</option>
+                        <option value="ITNM">Institut Teknologi Nasional Malang</option>
+                        <option value="UBNM">Universitas Bina Nusantara Malang</option>
+                        <option value="UB">Universitas Brawijaya</option>
+                        <option value="UG">Universitas Gajayana</option>
+                        <option value="UI-M">Universitas Islam Malang</option>
+                        <option value="UIN-M">Universitas Islam Negeri Maulana Malik Ibrahim</option>
+                        <option value="UKWKM">Universitas Katolik Widya Karya Malang</option>
+                        <option value="UNMER">Universitas Merdeka</option>
+                        <option value="UM">Universitas Negeri Malang</option>
+                        <option value="UMM">Universitas Muhammadiyah Malang</option>
+                        <option value="PGRI-M">Universitas PGRI Kanjuruhan Malang</option>
+                        <option value="UT-M">Universitas Terbuka Malang</option>
+                        <option value="UTT">Universitas Tribhuwana Tunggadewi</option>
+                        <option value="UWM">Universitas Widayagama Malang</option>
+                        <option value="UW-M">Universitas Wisnuwardhana Malang</option>
+                    </select>
+                    <label htmlFor="address" className="text-black text-base font-medium flex mt-[24px]">Alamat</label>
+                    <div className="flex flex-row">
+                        <div className="flex flex-col">
+                            <label htmlFor="city">Kota</label>
+                            <button>Malang</button>
+                        </div>
+                        <div className="flex flex-col">
+                            <label htmlFor="District">Kecamatan</label>
+                            <select name="district" id="district" required>
+                                <option value="" disabled selected>Pilih Kecamatan</option>
+                                <option value="blimbing">Blimbing</option>
+                                <option value="kedungkandang">Kedungkandang</option>
+                                <option value="klojen">Klojen</option>
+                                <option value="lowokwaru">Lowokwaru</option>
+                                <option value="sukun">Sukun</option>
+                            </select>
+                        </div>
+                    </div>
+                    <label htmlFor="interests"  className="text-black text-base font-medium flex mt-[24px]">Minat</label>
+
                 </form>
             </div>
         </div>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,47 @@
 import React from "react";
 import MainLayout from "../components/Layout/MainLayout";
+import Search from "../assets/icon/search.svg"
+import Line from "../assets/icon/line.svg"
+import Community from "../assets/icon/community.svg"
+import Person from "../assets/icon/person.svg"
 
 const MainPage = () => {
     return(
         <MainLayout>
-            <div>INI MAIN PAGE</div>
+            <div className="font-Poppins mx-[305px] mt-[61px]">
+                <div className="text-black text-xl font-semibold">Apa yang kamu butuhkan?</div>
+                <div className="mt-[24px] w-[830px] h-[49px] pl-5 pr-60 py-3 bg-white rounded-2xl border border-purple-900 justify-start items-center gap-2 inline-flex">
+                    <img src={Search} alt="Icon Search" className="w-[25px] h-[25px] relative"/>
+                    <div className="text-black text-opacity-30 text-base font-medium font-['Poppins']">Cari</div>
+                </div>
+                <div className="flex flex-row mt-[36px] gap-[20px]">
+                    <img src={Line} alt="Line Icon" />
+                    <p className="text-black text-opacity-30 text-base font-medium ">Atau</p>
+                    <img src={Line} alt="Line Icon" />
+                </div>
+                <div className="flex flex-row gap-[20px] mx-[108px]">
+                    <div className="w-[298px] mt-[36px] h-[332px] px-16 py-8 bg-white rounded-2xl border-2 border-purple-900 flex-col justify-start items-center gap-5 inline-flex">
+                        <div className="w-[120px] h-[120px] relative">
+                            <div className="w-[120px] h-[120px] left-0 top-0 absolute bg-white rounded-full border-2 border-purple-900" />
+                            <img src={Person} alt="Icon Person" className="w-[60px] h-[60px] left-[30px] top-[30px] absolute" />
+                        </div>
+                        <div className="text-center text-black text-xl font-semibold">Rekomendasi<br/>Partner</div>
+                        <button className="px-16 py-3 bg-purple-200 rounded-3xl justify-center items-center gap-2.5 inline-flex">
+                            <p className="text-center text-purple-900 text-xl font-bold ">Pilih</p>
+                        </button>
+                    </div>
+                    <div className="w-[298px] mt-[36px] h-[332px] px-16 py-8 bg-white rounded-2xl border-2 border-purple-900 flex-col justify-start items-center gap-5 inline-flex">
+                        <div className="w-[120px] h-[120px] relative">
+                            <div className="w-[120px] h-[120px] left-0 top-0 absolute bg-white rounded-full border-2 border-purple-900" />
+                            <img src={Community} alt="Icon Community" className="w-[60px] h-[60px] left-[30px] top-[30px] absolute" />
+                        </div>
+                        <div className="text-center text-black text-xl font-semibold mt-[43px]">Komunitas</div>
+                        <button className="px-16 py-3 bg-purple-200 rounded-3xl justify-center items-center gap-2.5 inline-flex mt-5">
+                            <p className="text-center text-purple-900 text-xl font-bold">Pilih</p>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </MainLayout>
     )
 }
