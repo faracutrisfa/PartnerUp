11004ad920466b5183b2602dca3e975f35db03a3
faracutrisfa <faracutrisfa@gmail.com>
2024-03-21 21:06:56 +0700

    update footer

--- src/components/Partials/footer.jsx
+++ src/components/Partials/footer.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
     return(
+        //Footer
         <footer className="mt-20">
             <div className="border border-black border-opacity-25 "></div>
             <div className="mx-[92px]">
