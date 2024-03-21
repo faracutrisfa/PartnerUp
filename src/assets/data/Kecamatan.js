ab3c47e8eda59ddb7b2e876df27183b1e1823d8a
faracutrisfa <faracutrisfa@gmail.com>
2024-03-21 21:24:54 +0700

    assets update

---
+++ src/assets/data/Kecamatan.js
@@ -0,0 +1,22 @@
+export const Kecamatan = [
+    {
+        district_id: 1,
+        text: "Blimbing"
+    },
+    {
+        district_id: 2,
+        text: "Kedungkandang"
+    },
+    {
+        district_id: 3,
+        text: "Klojen"
+    },
+    {
+        district_id: 4,
+        text: "Lowokwaru"
+    },
+    {
+        district_id: 5,
+        text: "Sukun"
+    },
+]
---
+++ src/assets/data/ListSkill.js
@@ -0,0 +1,22 @@
+export const ListSkill = [
+    {
+        skill_id: 1,
+        text: "Analisis Data"
+    },
+    {
+        skill_id: 2,
+        text: "Analisis Keuangan"
+    },
+    {
+        skill_id: 3,
+        text: "Bahasa Asing"
+    },
+    {
+        skill_id: 4,
+        text: "Public Speaking"
+    },
+    {
+        skill_id: 5,
+        text: "Time Management"
+    }
+]
